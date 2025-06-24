78111fad0d2b3eb18430a715667117b7d7dd9acb
ayman <danishaiman3b@gmail.com>
2025-06-24 22:09:10 +0800

    Disable debug banner in MaterialApp for a cleaner UI presentation.

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Contact Lens Reminder',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
           useMaterial3: true,
